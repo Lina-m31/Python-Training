efaacedb64f52573938b25fca3e07fca3cd5d8d2
Lina-m31 <57687193+Lina-m31@users.noreply.github.com>
2024-07-08 15:53:05 +0300

    Add files via upload

---
+++ Celestial_Explorer_v1/Main.py.py
@@ -0,0 +1,90 @@
+import View
+import constellation as cs
+
+def main ():
+
+##Creat a solar system using our solar system planets
+
+    solar = cs.solar_system()
+    solar_system_planets = [
+        cs.Planet("Mercury", 3.3011e23, 2439.7, 0.39, (0, 0, 0), 88, "Sun","gray50"),
+        cs.Planet("Venus", 4.8675e24, 6051.8, .72, (0, 0, 0), 224.7, "Sun","darkKhaki"),
+        cs.Planet("Earth", 5.972e24, 6371, 1, (0, 0, 0), 365.25, "Sun","blue"),
+        cs.Planet("Mars", 6.4171e23, 3389.5, 1.52, (0, 0, 0), 687, "Sun","red"),
+        cs.Planet("Jupiter", 1.8982e27, 69911, 5.2, (0, 0, 0), 4333, "Sun","chocolate"),
+        cs.Planet("Saturn", 5.6834e26, 58232, 9.54, (0, 0, 0), 10759, "Sun","khaki"),
+        cs.Planet("Uranus", 8.6810e25, 25362, 19.2, (0, 0, 0), 30687, "Sun","lightblue1"),
+        cs.Planet("Neptune", 1.02413e26, 24622, 30.6, (0, 0, 0), 60190, "Sun","deepskyblue"),
+        cs.Planet("Pluto", 1.30900e22, 1188.3, 39, (0, 0, 0), 90560, "Sun","brown1")  
+    ]
+
+    for planet in solar_system_planets:
+        solar.insert_planet(planet)
+
+
+
+    ## Creating constellation of 5 stars including the sun 
+
+    constellation=cs.Constellation("Sun",696340)
+    constellation.insert("Proxima Centauri",107280)
+    constellation.insert("Alpha Centauri B", 60268)
+    constellation.insert("Betelgeuse", 8870)
+    constellation.insert("sirius", 1190741)
+
+
+
+    # if you try to insert the sun you will recieve " star already added"
+    #constellation.insert("sun", 696340)
+
+
+
+
+    ## get the moon phase for a specific date   
+    def moon(date):
+
+        return(View.Moon_Phase(date))
+
+    date=input("please enter the date for the moon phase view:")
+    moon(date)
+
+
+#get the viw of a specific constellation
+
+    def const(date,name="ori"):
+
+        return(View.constellations(date,name))
+
+    date=input("please enter the date for the constellation view:")
+    name=input("please enter the constellation name:")
+    const(date,name)
+
+
+
+# Draw the constellation that we have created from 5 stars
+    def stars():
+        constellation.draw()
+
+# Draw our solar system planets
+    def planets():
+        solar.draw_planets()
+    
+#As user to choose what to view
+    x=True
+    while x:
+        user_input=input("choose to see the stars or planets? ")
+
+        if user_input == "stars" :
+            x=False
+            return stars()
+            
+    
+        elif user_input == "planets" :
+            x=False
+            return planets()
+        
+        else : 
+            return print ("invalid input") 
+            
+
+main()
+
---
+++ Celestial_Explorer_v1/constellation.py
@@ -0,0 +1,165 @@
+import math 
+import turtle
+
+
+#creat planets:
+class Planet:
+    
+    def __init__(self,name,mass,radius,Distance,velocity,Orbital_Period,star,color):
+
+        self.name=name
+        self.mass=mass
+        self.radius=radius
+        self.Distance=Distance
+        self.velocity=velocity
+        self.Orbital_Period=Orbital_Period
+        self.star=star
+        self.color=color
+    
+
+    def __str__(self): 
+        return f"planet  {self.name} that orbits {self.star} "
+    
+
+
+## add the planets to a list as solar_system and perfomr methods on them
+class solar_system:
+    planets = [] 
+    
+##Inserting planets to the solar_system
+    def __init__(self):
+        self.planets = []
+        
+
+    def insert_planet(self, planet):
+        self.planets.append(planet)
+
+    ## print planets names
+    def print_planets(self):
+        for planet in self.planets:
+            print(planet)
+
+    ## Draw the solar system using turtle 
+    def draw_planets(self):
+        t = turtle.Turtle()
+        screen = turtle.Screen()
+        screen.setworldcoordinates(-1000, -1000, 1000, 1000)
+        t.speed(0)
+        
+        ##use the class method from Constellation project to draw the sun
+        Constellation.draw_star(t, (6963.40*3) , (0, 0), "yellow")
+
+        for index, planet in enumerate(self.planets):
+            angle = 360 / len(self.planets) * index
+            distance_from_sun = 500 
+            x = distance_from_sun * math.cos(math.radians(angle))
+            y = distance_from_sun * math.sin(math.radians(angle))
+            t.penup()
+            t.goto(x, y)
+            t.pendown()
+            # Scale the planet radius for drawing and use the color 
+            t.dot(planet.radius/900 , planet.color)  
+            t.write(planet.name, align="center", font=("Arial", 8, "normal"))
+
+        
+        screen.exitonclick()
+
+
+
+#creat a star
+class Star:
+
+    position=(0,0,0)
+    velocity=(0,0,0)
+    number_of_stars=0
+
+    def __init__(self,name,radius):
+        self.name=name
+        self.radius=radius
+        self.smaller=None
+        self.bigger=None
+        Star.number_of_stars = Star.number_of_stars + 1
+
+
+    def __str__(self): 
+        return f"Star  {self.name}  "
+
+
+
+#Creat constellation of stars using tree method 
+class Constellation:
+    R=696340
+
+## Creat the main star , in our case it will be the sun
+    def __init__(self, name=None, radius=0):
+        self.root = Star(name,radius) if name else None
+        
+        
+ ## adding stars to the constellation
+    def insert(self, name, radius):
+        if self.root is None:
+            self.root = Star(name, radius)
+
+        else:
+            self._insert(self.root, name, radius)
+
+# The stars will be added based on its size compared to the sun, if smaller than the sun it will be added to smaller
+# if bigger it will be added to bigger 
+    def _insert(self, current_node, name, radius):
+
+        if radius < current_node.radius:
+            if current_node.smaller:
+                self._insert(current_node.smaller, name, radius)
+            else:
+                current_node.smaller = Star(name, radius)
+        elif radius > current_node.radius:
+            if current_node.bigger:
+                self._insert(current_node.bigger, name, radius)
+            else:
+                current_node.bigger = Star(name, radius)
+        else:
+            print("Star already added")
+
+
+
+
+    
+## methods to draw the stars based on its size
+    @classmethod
+    def draw_star(cls, t, radius, position , color):
+        t.penup()
+        t.goto(position)
+        t.pendown()
+        t.dot(radius/400 , color)
+    
+    def draw(self):
+        # Initialize turtle parameters
+        t = turtle.Turtle()
+        screen = turtle.Screen()
+        screen.setworldcoordinates(-1000, -1000, 1000, 1000)
+        t.speed(0)
+        
+        def _draw(current_node, position=(0, 0),level=0 , color="yellow"):
+            if current_node:
+                # Scale radius for drawing
+                new_radius = (current_node.radius / (Constellation.R/1000)) * 50
+                self.draw_star(t, new_radius, position , color)
+                
+                #defining offset to draw the stars seperatly
+                offset = 200 / (level + 1)
+                smaller_position = (position[0] - offset, position[1] - 100)
+                bigger_position = (position[0] + offset*3 , position[1] - 100)
+
+                # Draw smaller and bigger stars recursively
+                _draw(current_node.smaller, smaller_position, level + 1, "blue")
+                _draw(current_node.bigger, bigger_position, level + 1 , "red")
+        
+        _draw(self.root)  #which is the sun
+        screen.exitonclick()
+
+
+
+
+
+
+
